f32d5f52b768153655833543ae2ee75431116367
Elixir92i <erwan.chaume@gmail.com>
2021-05-12 11:33:32 +0200

    Added security (crypted mail/ helmet/ password)

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const apiLimiter = rateLimit({
+    windowMs: 15*60*1000,
+    max:100
+});
+
+mongoose.connect(process.env.DB,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/api/", apiLimiter);
+app.use(helmet());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+
+module.exports = app;
